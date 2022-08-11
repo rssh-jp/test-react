ba6bba5b279da7b278a77d0d12787b54ef944475
rssh-jp <t-araumi@rssh.sakura.ne.jp>
2022-08-11 23:18:35 +0900

    commit

---
+++ src/components/parts/TableRow/TableRow.test.tsx
@@ -0,0 +1,14 @@
+import { composeStories } from "@storybook/react"
+import {render} from '@testing-library/react'
+import {screen} from '@storybook/testing-library'
+
+import * as stories from './TableRow.stories'
+
+describe("TableRow", ()=>{
+    const {Default} = composeStories(stories)
+    test('default', ()=>{
+        render(<Default />)
+        const row = screen.getByRole('row')
+        expect(row.children.length).toEqual(3)
+    })
+})
