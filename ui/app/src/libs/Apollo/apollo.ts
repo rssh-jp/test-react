69df73a4db03cc4b7c60a574f8792a7b6b39ecdd
rssh-jp <t-araumi@rssh.sakura.ne.jp>
2022-08-14 00:37:14 +0900

    commit

--- ui/app/src/libs/Apollo/apollo.ts
+++ ui/app/src/libs/Apollo/apollo.ts
@@ -1,15 +1,15 @@
 import { ApolloClient, Cache, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
 
-let client:ApolloClient<NormalizedCacheObject> | null = null;
+let client: ApolloClient<NormalizedCacheObject> | null = null
 
-export function NewClient():ApolloClient<NormalizedCacheObject>{
+export function NewClient(): ApolloClient<NormalizedCacheObject> {
   return new ApolloClient({
     uri: 'http://localhost:8080/query',
     cache: new InMemoryCache(),
   })
 }
 
-export function ClientInstance():ApolloClient<NormalizedCacheObject>{
-    client = client == null ? NewClient(): client
-    return client
+export function ClientInstance(): ApolloClient<NormalizedCacheObject> {
+  client = client == null ? NewClient() : client
+  return client
 }
--- ui/app/src/pages/_app.tsx
+++ ui/app/src/pages/_app.tsx
@@ -3,13 +3,16 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ApolloProvider } from '@apollo/client'
 import { ClientInstance } from '../libs/Apollo/apollo'
+import SimpleSidebar from '../libs/Sidebar/Sidebar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = ClientInstance()
   return (
     <ChakraProvider>
       <ApolloProvider client={client}>
-        <Component {...pageProps} />
+        <SimpleSidebar>
+            <Component {...pageProps} />
+        </SimpleSidebar>
       </ApolloProvider>
     </ChakraProvider>
   )
